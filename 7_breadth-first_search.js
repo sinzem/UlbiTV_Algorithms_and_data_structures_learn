2a2c17034e7cf943c4c3b67b9a310073f7ed390e
Sergey <sinzem@gmail.com>
2024-09-28 00:44:23 +0300

    dijkstras_algorythm

--- 7_breadth-first_search.js
+++ 7_breadth-first_search.js
@@ -1,5 +1,6 @@
 /* (breadth-first search - поиск в ширину) */
 
+/* (в д.с граф представляет собой обьект, где ключ представляет собой вершину, а значение - массив с доступными соседними вершинами) */
 const graph = {};
 
 graph.a = ["b", "c"];
@@ -11,13 +12,13 @@ graph.f = ["g"];
 
 let cur = 0;
 
-function breadthSearch(graph, start, end) {
-    let queue = [];
-    queue.push(start);
+function breadthSearch(graph, start, end) { /* (передаем сам граф и точки старта и окончания маршрута) */
+    let queue = []; /* (очередь - массив с обрабатываемыми точками) */
+    queue.push(start); /* (закидываем стартовую точку) */
     while (queue.length > 0) {
         cur += 1;
-        // const current = queue.shift();
-        const current = queue.pop();
+        const current = queue.shift(); /* (удаляем из очереди стартовую точку, если в ее значениях содержится конечный результат, выдаем итог true, если нет - добавляем ее ключи в очередь и снова обрабатываем) */
+        // const current = queue.pop();
         if (!graph[current]) {
             graph[current] = [];
         }
@@ -32,3 +33,6 @@ function breadthSearch(graph, start, end) {
 
 console.log(breadthSearch(graph, "a", "g"));
 console.log("current: " + cur);
+/* (выдает результат true/false от точки start до end, автор работал через shift, но в д.с разницы нет shift или pop, функция сидьно упрощена, обещанный кратчайший путь не выводит, с поиском в ширину имеет мало общего) */
+
+
